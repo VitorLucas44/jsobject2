3eef05796978c4473e5c694e8555c5edcbb25b63
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-04 11:00:49 +0100

    parlerlog

--- public/js/main.js
+++ public/js/main.js
@@ -8,17 +8,24 @@ const vieille_dame = {
     objet: 'canne',
 parler: function() {
     if (this.moral === 'mal') {
-      alert(`Vous me dérangez et le frappe avec sa ${this.objet}`);
+    alert(`Vous me dérangez et le frappe avec sa ${this.objet}`);
     } else {
-      alert(`Bonjour ${this.nom.prenom} ${this.nom.nomFamille}`);
+    alert(`Bonjour ${this.nom.prenom} ${this.nom.nomFamille}`);
     }
-  }
+}
 };
 
-const vieille_homme  = {
+const vieille_homme = {
     age:80,
-    nom:{
-        prenom:'Asus',
-        nomfamille: 'laptop'
+    nom: {
+    prenom: 'Asus',
+    nomfamille: 'Laptop'
     },
-}
+    adoucir: function() {
+    vieille_dame.moral = 'bien';
+    }
+}; 
+
+vieille_dame.parler()
+
+console.log(vieille_dame)
