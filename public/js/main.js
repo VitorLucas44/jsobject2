5b071868559d0e425d4a58f017de70e9a58c6fae
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-04 10:56:06 +0100

    homme

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,24 @@
 const vieille_dame = {
     age: 80,
     nom: {
-    prenom: 'Berenice',
+    prenom: 'Assassin',
     nomfamille: 'Molengeek'
     },
     moral: 'mal',
     objet: 'canne',
+parler: function() {
+    if (this.moral === 'mal') {
+      alert(`Vous me dérangez et le frappe avec sa ${this.objet}`);
+    } else {
+      alert(`Bonjour ${this.nom.prenom} ${this.nom.nomFamille}`);
+    }
+  }
 };
+
+const vieille_homme  = {
+    age:80,
+    nom:{
+        prenom:'Asus',
+        nomfamille: 'laptop'
+    },
+}
